119fe0e07edb043b4ae10f541e8f73ec88d75553
fahminlb33 <fahmi@kodesiana.com>
2024-10-01 07:13:48 +0700

    feat: update training and eval scripts

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -17,14 +17,38 @@ features = [
 
 non_feature = ["zone_id", "ts"]
 
-df = pd.concat([
-  pd.read_parquet("../dataset/zonal/africa.parquet")[features], #.drop(columns=non_feature),
-  pd.read_parquet("../dataset/zonal/australia.parquet")[features], #.drop(columns=non_feature),
+UNDERSAMPLE = True
+SELECT_TOP  = False
+
+# load all
+all_dfs = pd.concat([
+  pd.read_parquet("../dataset/africa.parquet").assign(country="africa"),
+  pd.read_parquet("../dataset/australia.parquet").assign(country="australia"),
 ], ignore_index=True)
 
-df.info()
+if UNDERSAMPLE:
+  # determine lowest class
+  min_samples = min(
+      all_dfs[(all_dfs["target"] == 0) & (all_dfs["country"] == "africa")].shape[0],
+      all_dfs[(all_dfs["target"] == 1) & (all_dfs["country"] == "africa")].shape[0],
+      all_dfs[(all_dfs["target"] == 0) & (all_dfs["country"] == "australia")].shape[0],
+      all_dfs[(all_dfs["target"] == 1) & (all_dfs["country"] == "australia")].shape[0],
+  )
 
-df_train, df_temp = train_test_split(df, test_size=0.2, random_state=42, stratify=df["target"])
+  # undersample
+  sampled_dfs = [
+    all_dfs[(all_dfs["target"] == 0) & (all_dfs["country"] == "africa")].sample(min_samples, random_state=42),
+    all_dfs[(all_dfs["target"] == 1) & (all_dfs["country"] == "africa")].sample(min_samples, random_state=42),
+    all_dfs[(all_dfs["target"] == 0) & (all_dfs["country"] == "australia")].sample(min_samples, random_state=42),
+    all_dfs[(all_dfs["target"] == 1) & (all_dfs["country"] == "australia")].sample(min_samples, random_state=42),
+  ]
+  
+  df_final = pd.concat(sampled_dfs, ignore_index=True).drop(columns=["country"])
+else:
+  df_final = all_dfs
+
+# split train-test-validation
+df_train, df_temp = train_test_split(df_final, test_size=0.4, random_state=42, stratify=df_final["target"])
 df_test, df_validation = train_test_split(df_temp, test_size=0.5, random_state=42, stratify=df_temp["target"])
 
 print("Train:", len(df_train), "Validation:", len(df_validation), "Test:", len(df_test))
@@ -32,6 +56,16 @@ print("Train stats", df_train["target"].value_counts())
 print("Validation stats", df_validation["target"].value_counts())
 print("Test stats", df_test["target"].value_counts())
 
-df_train.to_parquet("../dataset/train-sel-10.parquet")
-df_validation.to_parquet("../dataset/validation-sel-10.parquet")
-df_test.to_parquet("../dataset/test-sel-10.parquet")
+if UNDERSAMPLE:
+  fsuffix = "-undersampled"
+else:
+  fsuffix = ""
+
+if SELECT_TOP:
+  df_train[features].to_parquet(f"../dataset/train-sel-10{fsuffix}.parquet")
+  df_validation[features].to_parquet(f"../dataset/validation-sel-10{fsuffix}.parquet")
+  df_test[features].to_parquet(f"../dataset/test-sel-10{fsuffix}.parquet")
+else:
+  df_train.to_parquet(f"../dataset/train-all{fsuffix}.parquet")
+  df_validation.to_parquet(f"../dataset/validation-all{fsuffix}.parquet")
+  df_test.to_parquet(f"../dataset/test-all{fsuffix}.parquet")
--- scripts/train.py
+++ scripts/train.py
@@ -1,13 +1,34 @@
+import time
+import json
+import pprint
+import random
 import argparse
-import datetime
-import itertools
 
 import numpy as np
 import pandas as pd
 
 import tensorflow as tf
+from sklearn.metrics import (
+    classification_report, 
+    matthews_corrcoef, 
+    accuracy_score, 
+    precision_score, 
+    recall_score, 
+    f1_score, 
+    roc_auc_score, 
+    average_precision_score,
+)
 
-print(tf.config.list_physical_devices('GPU'))
+# repro
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+gpus = tf.config.list_physical_devices('GPU')
+print(gpus)
+for gpu in gpus:
+	tf.config.experimental.set_memory_growth(gpu, True)
 
 EPOCHS = 200
 BATCH_SIZE = 256
@@ -23,7 +44,7 @@ def resblock(x, size):
 	
 	return out
 
-def create_model(kind, ds_train) -> tf.keras.Model:
+def create_model(kind, depth, ds_train) -> tf.keras.Model:
 	# create input layer
 	inputs = tf.keras.Input(shape=(ds_train.element_spec[0].shape[1],))
 
@@ -32,26 +53,19 @@ def create_model(kind, ds_train) -> tf.keras.Model:
 	norm_layer.adapt(ds_train.map(lambda x, _: x))
 
 	x = norm_layer(inputs)
-	# x = inputs
 
 	# create hidden layers
 	if kind == "fcn":
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
+		for _ in range(depth):
+			x = tf.keras.layers.Dense(128, activation='relu')(x)
 	elif kind == "resnet":
-		x = tf.keras.layers.Dense(128, activation='relu')(x)
-		x = resblock(x, 128)
-		x = resblock(x, 128)
-		x = resblock(x, 128)
-
-		x = resblock(x, 128)
-		x = resblock(x, 128)
-		x = resblock(x, 128)
+		x = tf.keras.layers.Dense(128, activation=None, use_bias=False, kernel_initializer=tf.keras.initializers.Ones())(x)
+		for _ in range(depth):
+			x = resblock(x, 128)
+	elif kind == "resnet_bias":
+		x = tf.keras.layers.Dense(128, activation="relu")(x)
+		for _ in range(depth):
+			x = resblock(x, 128)
 	else:
 		raise ValueError(f"Invalid kind: {kind}")
 
@@ -62,33 +76,30 @@ def create_model(kind, ds_train) -> tf.keras.Model:
 	return tf.keras.Model(inputs=inputs, outputs=outputs, name=f"model_{kind}")
 
 def create_loss(lf):
-	if lf == "bc":
+	if lf == "bce":
 		return tf.keras.losses.BinaryCrossentropy(from_logits=False)
 	
 	return tf.keras.losses.BinaryFocalCrossentropy(gamma=2, from_logits=False)
 
 def main(args):
-	loss_name = args.loss
-	model_name = args.model
-	dataset_suffix = args.dataset
+	model_file_name = f"{args.dataset_name}_{args.model}_{args.loss}_{args.depth}"
 
 	print("Loading data...")
-	arr_train = pd.read_parquet(f"../dataset/train-{dataset_suffix}.parquet").values
-	arr_test = pd.read_parquet(f"../dataset/test-{dataset_suffix}.parquet").values
-	arr_val = pd.read_parquet(f"../dataset/validation-{dataset_suffix}.parquet").values
+	arr_train = pd.read_parquet(f"../dataset/train-{args.dataset_name}.parquet").drop(columns=["country", "zone_id", "ts"], errors="ignore").values
+	arr_test = pd.read_parquet(f"../dataset/test-{args.dataset_name}.parquet").drop(columns=["country", "zone_id", "ts"], errors="ignore").values
+	arr_val = pd.read_parquet(f"../dataset/validation-{args.dataset_name}.parquet").drop(columns=["country", "zone_id", "ts"], errors="ignore").values
 
 	print("Creating tf.data...")
 	ds_train = tf.data.Dataset.from_tensor_slices((arr_train[:, :-1], arr_train[:, -1])).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 	ds_val = tf.data.Dataset.from_tensor_slices((arr_val[:, :-1], arr_val[:, -1])).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-	ds_test = tf.data.Dataset.from_tensor_slices((arr_test[:, :-1], arr_test[:, -1])).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 	
 	print("Creating model...")
-	model = create_model(model_name, ds_train)
+	model = create_model(args.model, args.depth, ds_train)
 
 	print("Compiling model...")
 	model.compile(
 		optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-		loss=create_loss(loss_name),
+		loss=create_loss(args.loss),
 		metrics=[
 			tf.keras.metrics.TruePositives(name='tp'),
 			tf.keras.metrics.FalsePositives(name='fp'),
@@ -111,30 +122,64 @@ def main(args):
 		mode='max',
 		restore_best_weights=True)
 
-	log_dir = f"logs/fit/model-{dataset_suffix}_{model_name}_{loss_name}"
+	log_dir = f"logs/{model_file_name}"
 	tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 	print("Training...")
+	train_start = time.time()
 	model.fit(
 		ds_train,
 		epochs=EPOCHS,
 		validation_data=ds_val,
-		callbacks=[early_stopping, tensorboard_callback])
+		verbose=2,
+		callbacks=[tensorboard_callback, early_stopping])
+	train_elapsed = time.time() - train_start
 	
 	print("Evaluate on test data")
-	results = model.evaluate(ds_test)
-	print("test loss, test acc:", results)
+	X_test = arr_test[:, :-1]
+	y_test = arr_test[:, -1].astype(int)
+
+	test_start = time.time()
+	y_pred = model.predict(X_test, verbose=2)
+	y_pred = np.where(y_pred > 0.5, 1, 0).ravel()
+	test_elapsed = time.time() - test_start
+
+	metrics = {
+		"params_model": args.model, 
+		"params_loss": args.loss, 
+		"params_depth": args.depth, 
+		"params_dataset": args.dataset_name, 
+
+		"training_time": train_elapsed,
+		"inference_time": test_elapsed,
+		"mcc": matthews_corrcoef(y_test, y_pred),
+		"accuracy": accuracy_score(y_test, y_pred),
+		"precision": precision_score(y_test, y_pred),
+		"recall": recall_score(y_test, y_pred),
+		"f1": f1_score(y_test, y_pred),
+		"roc_auc": roc_auc_score(y_test, y_pred),
+		"ap": average_precision_score(y_test, y_pred),
+	}
+
+	pprint.pprint(metrics)
+	print(classification_report(y_test, y_pred))
+
+	with open("metrics.jsonl", "a+") as f:
+		json.dump(metrics, f)
+		f.write("\n")
 	
 	print("Saving model...")
-	model.save(f"models/model-{dataset_suffix}_{model_name}_{loss_name}.keras")
+	model.save(f"models/{model_file_name}.keras")
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog="autogbifml")
-	parser.add_argument("-m", "--model")
-	parser.add_argument("-l", "--loss")
-	parser.add_argument("-d", "--dataset")
+	parser.add_argument("-m", "--model", choices=["fcn", "resnet", "resnet_bias"])
+	parser.add_argument("-l", "--loss", choices=["bce", "fl"])
+	parser.add_argument("-d", "--depth", type=int)
+	parser.add_argument("-n", "--dataset-name")
 
 	args = parser.parse_args()
+	print(repr(args))
 	
 	main(args)
